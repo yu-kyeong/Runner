7d9920f45d97d66da21151458985b1a7f30b62b4
yu_kyeong <qjelmg.g@gmail.com>
2021-05-15 00:53:51 +0900

    yu_kyeong : 러닝시작/종료 구현 , Google Map 위치변동 이벤트 구현중

--- app/src/main/java/com/kyeong/runner/MapActivity.kt
+++ app/src/main/java/com/kyeong/runner/MapActivity.kt
@@ -2,16 +2,22 @@ package com.kyeong.runner
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 
 class MapActivity : AppCompatActivity() {
 
@@ -22,6 +28,14 @@ class MapActivity : AppCompatActivity() {
 
     lateinit var map : GoogleMap
     lateinit var locationManager : LocationManager
+
+    //러닝 시작유무
+    var runState : Boolean = false
+
+    //위치
+    var position : LatLng = LatLng(0.0,0.0)
+    var startPos : LatLng = LatLng(0.0, 0.0)
+    var endPos : LatLng = LatLng(0.0 , 0.0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -92,21 +106,41 @@ class MapActivity : AppCompatActivity() {
             Log.d("test", "onMapReady")
             map = p0
             getMyLocation()
+
+
         }
     }
+
+    //움직이는 위치 그리기
+    fun drawPath(){
+        Log.d("State","Draw")
+        val option : PolylineOptions = PolylineOptions()
+            .add(startPos).add(endPos).color(Color.BLUE).width(12f).geodesic(true)
+        map.addPolyline(option)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(startPos,18f))
+
+    }
+
+
+
 
     //내위치 측정하기
     open fun setMyLocation(location : Location){
         Log.d("Location","위도 : "+location.latitude)
         Log.d("Location", "경도 : "+location.longitude)
 
-        val position : LatLng = LatLng(location.latitude , location.longitude)
+        position = LatLng(location.latitude , location.longitude)
 
         val update1 : CameraUpdate = CameraUpdateFactory.newLatLng(position)
         val update2 : CameraUpdate = CameraUpdateFactory.zoomTo(15f)
 
         map.moveCamera(update1)
         map.animateCamera(update2)
+
+        /*map.addMarker(
+            MarkerOptions()
+                .position(position)
+        ) 현재위치 마커표시 */
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
@@ -115,7 +149,20 @@ class MapActivity : AppCompatActivity() {
         }
 
         //현재위치 표시
-        map.isMyLocationEnabled
+        map.isMyLocationEnabled = true
+
+        val locationBtn : Button = findViewById(R.id.location_click)
+        locationBtn.setOnClickListener {
+           // map.setOnMyLocationClickListener {
+                if (map.isMyLocationEnabled == true) {
+                    map.setOnMyLocationButtonClickListener { false }
+                    map.isMyLocationEnabled = false
+                } else {
+                    map.setOnMyLocationButtonClickListener { true }
+                    map.isMyLocationEnabled = true
+                }
+         //   }
+        }
 
     }
 
@@ -158,11 +205,49 @@ class MapActivity : AppCompatActivity() {
 
     }
 
+    fun runningState(p0: Location) {
+        if (runState == false){
+            Toast.makeText(this@MapActivity, "러닝 시작", Toast.LENGTH_SHORT).show()
+            runState = true
+
+            while (runState) {
+
+                Log.d("State", "Lat : "+p0.latitude+"\t Log : "+p0.longitude)
+                p0.latitude = p0.latitude+20.0
+                p0.longitude = p0.longitude+20.0
+
+                return
+            }
+        }else{
+            Toast.makeText(this@MapActivity, "러닝 종료", Toast.LENGTH_SHORT).show()
+            runState = false
+        }
+    }
+
     inner class GetMyLocationListener : LocationListener {
         override fun onLocationChanged(p0: Location) {
             setMyLocation(p0)
             locationManager.removeUpdates(this)
-        }
+            val startBtn : Button = findViewById(R.id.start_btn)
+            startBtn.setOnClickListener {
+                runningState(p0)
+            }
+
+
+            position = LatLng(p0.latitude, p0.longitude)
+
+            var lat : Double = p0.latitude
+            var lon = p0.longitude
+
+
+            val movePosition : CameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(lat,lon),18f)
+            map.animateCamera(movePosition)
+                if (runState){
+                    endPos = LatLng(lat,lon)
+                    drawPath()
+                    startPos = LatLng(lat,lon)
+                }
+            }
 
         override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
         }
@@ -173,4 +258,5 @@ class MapActivity : AppCompatActivity() {
         override fun onProviderDisabled(provider: String) {
         }
     }
+
 }

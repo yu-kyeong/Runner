1ab5bdf8562c7ad783731ffac0fcdf0c32b6e8db
yu_kyeong <qjelmg.g@gmail.com>
2021-06-10 18:11:55 +0900

    yu_kyeong : 이메일,비밀번호 유효성 검사 완료

--- app/src/main/java/com/kyeong/runner/SignUpActivity.kt
+++ app/src/main/java/com/kyeong/runner/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.kyeong.runner
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -11,7 +12,12 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import android.text.TextWatcher
+import android.view.View
+import androidx.core.view.isInvisible
 import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -21,34 +27,133 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        //회원가입 작성란
         val email: EditText = findViewById(R.id.new_id)
         val password: EditText = findViewById(R.id.new_pwd)
         val rePwd: EditText = findViewById(R.id.re_pwd)
         val nickName: EditText = findViewById(R.id.new_nick)
         val sex: RadioGroup = findViewById(R.id.radio_group)
 
+        //회원가입 버튼
         val signUp: Button = findViewById(R.id.newSign_up)
+
+        //유효성 검사
+        val emailError : TextView = findViewById(R.id.email_error)
+        val passwordError : TextView = findViewById(R.id.password_error)
+        val rePasswrodError : TextView = findViewById(R.id.rePassword_error)
 
         auth = Firebase.auth
 
+
         email.addTextChangedListener(object :TextWatcher{
+            //입력 전
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
+                return
             }
 
+            //입력 중
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
+                Log.d("signUpActivity","onTextChanged 중")
+                var pattern : Pattern = Pattern.compile("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")
+                var compile : Matcher = pattern.matcher(email.text.toString())
+
+                if (compile.matches()){
+                    Log.d("signUpActivity","if 들어옴")
+                    email.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.colorAccent
+                    )
+                    emailError.visibility = View.GONE
+
+                }
+                if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches() ){
+                    Log.d("signUpActivity","! if 들어옴")
+                    email.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.changeEdittext
+                    )
+                    emailError.visibility = View.VISIBLE
+                    return
+                }
             }
 
+
+            //입력 후
             override fun afterTextChanged(p0: Editable?) {
-                TODO("Not yet implemented")
+                if (email.text.toString().length == 0){
+                    Log.d("signUpActivity","afterTextChanged")
+                    email.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.colorAccent
+                    )
+                    emailError.visibility = View.GONE
+                }
+                return
             }
         })
 
         isValidMail(email)
 
+        password.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", password.text.toString()) ){
+                    Log.d("signUpActivity","password if 들어옴")
+                    password.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.colorAccent
+                    )
+                    passwordError.visibility = View.GONE
+                }
+                if(!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", password.text.toString())){
+                    Log.d("signUpActivity","password !if 들어옴")
+                    password.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.changeEdittext
+                    )
+                    passwordError.visibility = View.VISIBLE
+                }
+            }
 
+            override fun afterTextChanged(p0: Editable?) {
+                if (password.text.toString().length == 0){
+                    Log.d("signUpActivity","password after 들어옴")
+                    password.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.colorAccent
+                    )
+                    passwordError.visibility = View.GONE
+                }
+            }
+        })
+
+        rePwd.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (password.text.toString() == rePwd.text.toString()){
+                    rePwd.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.colorAccent
+                    )
+                    rePasswrodError.visibility = View.GONE
+                }
+                if(password.text.toString() != rePwd.text.toString()){
+                    rePwd.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.changeEdittext
+                    )
+                    rePasswrodError.visibility = View.VISIBLE
+                }
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+                if (rePwd.text.toString().length == 0){
+                    Log.d("signUpActivity","rePassword after 들어옴")
+                    rePwd.backgroundTintList = applicationContext.getColorStateList(
+                        R.color.colorAccent
+                    )
+                    rePasswrodError.visibility = View.GONE
+                }
+            }
+        })
 
         signUp.setOnClickListener {
             Log.d("signUpActivity", "onClick")
@@ -109,7 +214,7 @@ class SignUpActivity : AppCompatActivity() {
                             // If sign in fails, display a message to the user.
                             Log.w("signUpActivity", "createUserWithEmail:failure", task.exception)
                             Toast.makeText(
-                                baseContext, "Authentication failed.",
+                                baseContext, "회원가입에 실패하였습니다",
                                 Toast.LENGTH_SHORT
                             ).show()
                             updateUI(null)
@@ -134,6 +239,20 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun updateUI(user: FirebaseUser?) {
 
+    }
+
+    class textWatcher : TextWatcher{
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+            TODO("Not yet implemented")
+        }
     }
 
 }
